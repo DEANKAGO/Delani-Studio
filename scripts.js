52a6b533bf765888c796ab3a762f3b512e4704cf
Dean Kago <martokhago@gmail.com>
2022-03-13 15:33:19 +0300

    added toggle for devep

--- scripts.js
+++ scripts.js
@@ -2,10 +2,14 @@ $(document).ready(function(){
   $("#design-main").click(function(){
     $("#design-img").toggle();
     $("#design").toggle();
+  })
 
+  $("#development-main").click(function(){
+    $("#development-img").toggle();
+    $("#development").toggle();
+  })
 
-
-
-
+  $("#product").click(function(){
+    $("#product-img").toggle();
   })
 })
